e1048e24c0dc686b5102349ac2c50056181fafc4
Arvinmoj <arvinmojaverian@hotmail.com>
2022-09-01 01:14:18 +0430

    setup dependency container

---
+++ src/Server/Core/DependencyContainer.cs
@@ -0,0 +1,126 @@
+﻿using MediatR;
+using FluentValidation;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core;
+public static class DependencyContainer
+{
+
+    #region Constructor
+    static DependencyContainer()
+    {
+    }
+    #endregion / Constructor
+
+    #region Configure Services
+    public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+    {
+        // Add HttpContextAccessor
+        #region HttpContextAccessor
+        services.AddTransient
+             <IHttpContextAccessor, HttpContextAccessor>();
+        #endregion / HttpContextAccessor
+
+        // Add MediatR
+        #region MediatR
+
+        // services.AddMediatR
+        //         (typeof(Application.MappingProfile).GetTypeInfo().Assembly);
+
+        #endregion / MediatR
+
+        // Add FluentValidation
+        #region Fluent Validation
+
+        // services.AddValidatorsFromAssembly
+        //         (assembly: typeof(Application.Commands.User.CreateProfileCommandValidator).Assembly);
+
+        #endregion / Fluent Validation
+
+        // Add MediatR PipeLine
+        #region MediatR PipeLine
+
+        // services.AddTransient
+        //         (typeof(IPipelineBehavior<,>),
+        //          typeof(Application.Behavior.ValidationBehavior<,>));
+
+        #endregion / MediatR PipeLine
+
+        // Add Auto Mapper
+        #region Auto Mapper
+
+        // services.AddAutoMapper
+        //         (profileAssemblyMarkerTypes: typeof(Application.MappingProfile));
+
+        #endregion / Auto Mapper
+
+        // Add Unit Of Work
+        #region Unit Of Work
+
+        // services.AddTransient<Persistence.IUnitOfWork, Persistence.UnitOfWork>(current =>
+        // {
+        //     string databaseConnectionString =
+        //         configuration
+        //         .GetSection(key: "ConnectionStrings")
+        //         .GetSection(key: "CommandsConnectionString")
+        //         .Value;
+
+        //     string databaseProviderString =
+        //         configuration
+        //         .GetSection(key: "CommandsDatabaseProvider")
+        //         .Value;
+
+        //     Infrastructure.Enumerations.Provider databaseProvider =
+        //         (Infrastructure.Enumerations.Provider)
+        //         System.Convert.ToInt32(databaseProviderString);
+
+        //     Infrastructure.Options options =
+        //         new Infrastructure.Options
+        //         {
+        //             Provider = databaseProvider,
+        //             ConnectionString = databaseConnectionString,
+        //         };
+
+        //     return new Persistence.UnitOfWork(options: options);
+        // });
+
+        #endregion / Unit Of Work
+
+        // Add Query Unit Of Work
+        #region Query Unit Of Work
+
+        // services.AddTransient<Persistence.IQueryUnitOfWork, Persistence.QueryUnitOfWork>(current =>
+        // {
+        //     string databaseConnectionString =
+        //         configuration
+        //         .GetSection(key: "ConnectionStrings")
+        //         .GetSection(key: "QueriesConnectionString")
+        //         .Value;
+
+        //     string databaseProviderString =
+        //         configuration
+        //         .GetSection(key: "QueriesDatabaseProvider")
+        //         .Value;
+
+        //     Infrastructure.Enumerations.Provider databaseProvider =
+        //         (Infrastructure.Enumerations.Provider)
+        //         System.Convert.ToInt32(databaseProviderString);
+
+        //     Infrastructure.Options options =
+        //         new Infrastructure.Options
+        //         {
+        //             Provider = databaseProvider,
+        //             ConnectionString = databaseConnectionString,
+        //         };
+
+        //     return new Persistence.QueryUnitOfWork(options: options);
+        // });
+
+        #endregion / Query Unit Of Work
+    }
+    #endregion / Configure Services
+
+}

4f7441ebdc8333302ef5b2a81facec542e96f878
Arvinmoj <arvinmojaverian@hotmail.com>
2022-08-31 15:41:35 +0430

    setup logging library

---
+++ src/Server/Logging/Log4NetAdapter.cs
@@ -0,0 +1,15 @@
+namespace Logging
+{
+    public class Log4NetAdapter<T> : Logger<T> where T : class
+    {
+        public Log4NetAdapter
+            (Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+
+        protected override void LogByFavoriteLibrary(Log log, System.Exception exception)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

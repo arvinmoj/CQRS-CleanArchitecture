aacddf78c6ea247d84dff35c7a307f8ecb4505c2
Arvinmoj <arvinmojaverian@hotmail.com>
2022-09-04 15:41:07 +0430

    Add program service builder

--- src/Server/API/Program.cs
+++ src/Server/API/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+Core.DependencyContainer.ConfigureServices
+            (configuration: builder.Configuration, services: builder.Services);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
